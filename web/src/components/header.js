2004d1d16d1290ef12eadba1433e5c70536bc2c3
RyKilleen <rykilleen@gmail.com>
2020-06-27 22:35:15 -0400

    progress on nav

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -6,53 +6,80 @@ import {cn} from '../lib/helpers'
 import styles from './header.module.css'
 
 const createLanguageLinks = (currentLocale, currentLocation) => {
-  const supportedLocales = ['en', 'es', 'zh']
+  const supportedLocales = [
+    {locale: 'en', label: 'English'},
+    {locale: 'es', label: 'Español'},
+    {locale: 'zh', label: '中文'}
+  ]
 
   return supportedLocales
-    .filter(loc => loc !== currentLocale)
-    .map(loc => {
-      const url = currentLocation.replace(currentLocale, loc)
+    .filter(localeObj => localeObj.locale !== currentLocale)
+    .map(localeObj => {
+      const url = currentLocation.replace(currentLocale, localeObj.locale)
       return (
         <li key={url}>
-          <Link to={url}>{loc}</Link>
+          <Link to={url} className={styles.languageLink}>
+            {localeObj.label}
+          </Link>
         </li>
       )
     })
 }
 
 const createCTA = (cta, isMain) => {
-  const classes = [styles.cta]
-  if (isMain) classes.push(styles.mainCTA)
-  return <li>
-    <a href={cta.href} className={classes.join(' ')} >{cta.text}</a>
-  </li>
+  const classes = isMain ? styles.mainCTA : styles.cta
+
+  return (
+    <li>
+      <a href={cta.href} className={classes}>
+        {cta.text}
+      </a>
+    </li>
+  )
 }
 
-const Header = ({onHideNav, onShowNav, showNav, siteTitle, slugs, currentLocale, location, ctas}) => (
+const Header = ({
+  onHideNav,
+  onShowNav,
+  showNav,
+  siteTitle,
+  slugs,
+  currentLocale,
+  location,
+  ctas
+}) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
-      <div className={styles.branding}>
-        <Link to={`/${currentLocale}`}>{siteTitle}</Link>
-      </div>
-
-      <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
-      </button>
+      <Link to={`/${currentLocale}`}>{siteTitle}</Link>
+      <nav className={styles.mobileNavContainer}>
+        <button className={cn(styles.cta, styles.toggleNavButton)} onClick={showNav ? onHideNav : onShowNav}>
+        Menu
+        </button>
+        <button className={cn(styles.darkCTA, styles.backToTopButton)}>
+        Back to Top
+        </button>
+      </nav>
 
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>{createLanguageLinks(currentLocale, location.pathname)}</ul>
-        <ul>
-          {ctas.secondaryCTAs && ctas.secondaryCTAs.map(cta => createCTA(cta))}
-          {ctas.mainCTA && createCTA(ctas.mainCTA, true)}
-        </ul>
-        <ul>
-          {slugs &&
-            slugs.map(slug => (
-              <li key={slug}>
-                <Link to={`/${currentLocale}/${slug}/`}>About</Link>
-              </li>
-            ))}
-        </ul>
+
+        <nav className={styles.languageNav}>
+          <ul className={styles.languageList}>{createLanguageLinks(currentLocale, location.pathname)}</ul>
+        </nav>
+        <nav className={styles.ctaNav}>
+          <ul>
+            {ctas.secondaryCTAs && ctas.secondaryCTAs.map(cta => createCTA(cta))}
+            {ctas.mainCTA && createCTA(ctas.mainCTA, true)}
+          </ul>
+          <ul>
+            {slugs &&
+              slugs.map(slug => (
+                <li key={slug}>
+                  <Link to={`/${currentLocale}/${slug}/`}>About</Link>
+                </li>
+              ))}
+          </ul>
+        </nav>
+        <button className={cn(styles.cta, styles.navClose, styles.darkCTA)} onClick={showNav ? onHideNav : onShowNav}>Close</button>
       </nav>
     </div>
   </div>
